111fb224786bb8f5baafdb8b0a5e73581a44cda4
emoe99979-byte <emoe99979@gmail.com>
2026-02-12 06:06:12 -0500

    initial commit

---
+++ ty.py
@@ -0,0 +1,81 @@
+#!/bin/python3
+                        ##string
+# i am print here string
+print('Hello World')
+print ("Hello World")
+print("""hello use
+-- hello user2""") # i used multiline for triple quote
+print("my chanel name"+"coder-web")
+print("\n") #this to separate the line
+print("Hello")
+
+#-----------------------------------------------------------------
+
+
+                      ##MATH
+print(200+200)#add
+print(300-100)#subtract
+print(2*10)#multiply
+print(100/100)#divide
+print(5+2*3)#PENDAS
+print(100**2)
+print(10%3)#MODULO
+print(10/3)#division with float
+print(10//3)#no float number
+
+#-------------------------------------------------------------------
+                            ##variables and Me
+#username ="ALI"
+#print(username)
+channelname ="coder web"
+print(channelname.upper())#uppercase
+print(channelname.lower())#lowercase
+print(channelname.title())#titlecase
+print(len(channelname))
+
+name ="Ahmed"
+age =23 #integar
+degree =49.5 #decimal or float
+print(name)
+print(23)
+print(49.5)
+print(int(degree))#remove the float number
+
+
+age +=1 
+print(age)
+birthday = 1
+age +=1 
+print(age)
+print("My name is"+ name + "and my age is"+str(age))
+#-----------------------------------------------------------------
+                      #Functions 
+def aboutyou() :
+         name2 = "Ahmed"#locale variable 
+         age = 23 #locale variable
+print("My name is"+ name + "and my age is"+str(age))
+
+aboutyou()
+
+
+def add(number) :
+        print(number+200)
+add(100)
+add(200)
+
+
+def addtwonumbers(num1,num2) :
+        print(num1+num2)
+addtwonumbers(400,400)
+
+def multiply(num1,num2) :
+        return num1 * num2 
+multiply(2,2)
+def multiply2(num1,num2):
+      result = num1* num2
+      return result
+store = multiply2 (2,2)
+print( store +4)
+def nl() ;
+      print("\n")
+nl()
